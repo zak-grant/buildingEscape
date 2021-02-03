0753498573c3226b642b9a01f96f99bc06b96828
terrazakdel <zak.d.grant@gmail.com>
2021-02-03 15:32:32 -0500

    Initial Commit

---
+++ Source/BuilingEscape/BuilingEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuilingEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILINGESCAPE_API ABuilingEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
